4c0094d2df66404ac5c5cc6c0d0c85f4906b9bc6
liuh <844348677@qq.com>
2016-03-01 17:34:19 +0800

    1

---
+++ src/main/scala/TestColor.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by liuh on 2016/3/1.
+  */
+object TestColor extends Enumeration{
+ // val Red = value(0,"stop")
+  val Red = Value(0,"stop")
+  val Yellow = Value(10)
+  val Green = Value("go")
+  def main(args: Array[String]) {
+    println("1")
+
+  }
+}
